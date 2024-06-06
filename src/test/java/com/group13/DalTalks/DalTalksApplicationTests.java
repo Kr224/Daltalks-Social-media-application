2f25ff6d9f5f01179e371fbe460657657874a8d4
Ariya Moez Dhukka <ar584283@dal.ca>
2024-06-06 18:09:52 -0300

    feat: Uploaded Spring Boot backend

---
+++ src/test/java/com/group13/DalTalks/DalTalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group13.DalTalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalTalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
