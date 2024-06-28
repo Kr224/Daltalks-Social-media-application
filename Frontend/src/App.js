7cdc4977d28c33aa6b1a9e8710ff30ac905eb8e3
Manvi Aggarwal <mn863120@dal.ca>
2024-06-27 20:44:30 -0700

    Changes added to frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/signUp';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import ForgotPassword from './components/forgotPassword';
-=======
 import FriendRequests from './components/FriendRequests';
->>>>>>> main
-=======
 import ErrorPage from './components/ErrorPage';
 import PrivateRoute from './components/PrivateRoute';
 import ProfilePage from './components/ProfilePage';
 import FriendRequestComponent from './components/FriendRequestComponent';
 import './App.css';
->>>>>>> main
 
 const App = () => {
     return (
@@ -22,18 +16,12 @@ const App = () => {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
                 <Route path="/forgotPassword" element={<ForgotPassword />} />
-=======
                 <Route path="/friend-requests" element={<FriendRequests />} />
->>>>>>> main
-=======
                 <Route path="/error" element={<ErrorPage />} />
                 <Route path="/friend-requests" element={<FriendRequestComponent />} />
                 <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/profile" element={<PrivateRoute component={ProfilePage} />} />
->>>>>>> main
             </Routes>
         </Router>
     );
