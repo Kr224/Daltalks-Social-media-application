ac35b1a5f161b9a093f77d82d562cef2701f8f46
Ariya Moez Dhukka <ar584283@dal.ca>
2024-06-28 11:34:16 -0300

    fix: attempting to fix errors trial 1

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/signUp';
+<<<<<<< HEAD
 import FriendRequests from './components/FriendRequests';
+=======
+import ErrorPage from './components/ErrorPage';
+import PrivateRoute from './components/PrivateRoute';
+>>>>>>> parent of 2ba4b85 (fix: fixed all merge conflicts)
 
 const App = () => {
     return (
@@ -10,7 +15,15 @@ const App = () => {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+<<<<<<< HEAD
                 <Route path="/friend-requests" element={<FriendRequests />} />
+=======
+                <Route path="/error" element={<ErrorPage />} />
+                <Route
+                    path="/"
+                    element={<Navigate to="/login" />}
+                />
+>>>>>>> parent of 2ba4b85 (fix: fixed all merge conflicts)
             </Routes>
         </Router>
     );
