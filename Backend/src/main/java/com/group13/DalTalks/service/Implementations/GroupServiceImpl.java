f189ae7c9ad73cc2c6deacc4c2f40ad6f4945bf2
Danielle Clark <dn449970@dal.ca>
2024-07-17 13:57:36 -0300

    test: created boilerplate code for creating group and test for basic group creation for TDD development

--- Backend/src/main/java/com/group13/DalTalks/service/Implementations/GroupServiceImpl.java
+++ Backend/src/main/java/com/group13/DalTalks/service/Implementations/GroupServiceImpl.java
@@ -1,6 +1,11 @@
 package com.group13.DalTalks.service.Implementations;
 
+import com.group13.DalTalks.model.Group;
 import com.group13.DalTalks.service.GroupService;
 
 public class GroupServiceImpl implements GroupService {
+
+  public Group createGroup(Group group) {
+    return null;
+  }
 }
