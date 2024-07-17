0bc39fb5410371314c427495e23e66b1c1e3bde1
Danielle Clark <dn449970@dal.ca>
2024-07-17 15:05:34 -0300

    feat: added create group support to controller and service layer

--- Backend/src/main/java/com/group13/DalTalks/controller/GroupController.java
+++ Backend/src/main/java/com/group13/DalTalks/controller/GroupController.java
@@ -1,11 +1,19 @@
 package com.group13.DalTalks.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.group13.DalTalks.model.Group;
+import com.group13.DalTalks.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/groups")
 public class GroupController {
+  @Autowired
+  private GroupService groupService;
+
+  @PostMapping("/create-group")
+  public Group createGroup(@RequestBody Group group) {
+    return groupService.createGroup(group);
+  }
 }
--- Backend/src/main/java/com/group13/DalTalks/service/GroupService.java
+++ Backend/src/main/java/com/group13/DalTalks/service/GroupService.java
@@ -1,4 +1,7 @@
 package com.group13.DalTalks.service;
 
+import com.group13.DalTalks.model.Group;
+
 public interface GroupService {
+  Group createGroup(Group group);
 }
