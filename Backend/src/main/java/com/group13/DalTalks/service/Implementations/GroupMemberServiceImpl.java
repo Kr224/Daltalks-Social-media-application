b53fb9a25b9e23615f0a16e07de25418c1901c13
Danielle Clark <dn449970@dal.ca>
2024-07-18 12:48:17 -0300

    feat: fixed failing test by adding functionality to save a group membership

--- Backend/src/main/java/com/group13/DalTalks/service/Implementations/GroupMemberServiceImpl.java
+++ Backend/src/main/java/com/group13/DalTalks/service/Implementations/GroupMemberServiceImpl.java
@@ -1,15 +1,22 @@
 package com.group13.DalTalks.service.Implementations;
 
 import com.group13.DalTalks.model.GroupMembers;
+import com.group13.DalTalks.repository.GroupMemberRepository;
+import com.group13.DalTalks.repository.GroupRepository;
 import com.group13.DalTalks.service.GroupMemberService;
+import jakarta.persistence.Access;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GroupMemberServiceImpl implements GroupMemberService {
   //here we implement the interface methods
 
+  @Autowired
+  private GroupMemberRepository groupMemberRepository;
+
   @Override
   public GroupMembers saveGroupMember(GroupMembers groupMembers) {
-    return null;
+    return groupMemberRepository.save(groupMembers);
   }
 }
