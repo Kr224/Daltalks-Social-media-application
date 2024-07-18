a2707730057ba0a28f1d9221f08acd65b177becd
Danielle Clark <dn449970@dal.ca>
2024-07-18 12:53:02 -0300

    feat: added functionality for adding group member to controller

--- Backend/src/main/java/com/group13/DalTalks/controller/GroupMemberController.java
+++ Backend/src/main/java/com/group13/DalTalks/controller/GroupMemberController.java
@@ -1,18 +1,23 @@
 package com.group13.DalTalks.controller;
 
 
+import com.group13.DalTalks.model.GroupMembers;
 import com.group13.DalTalks.repository.GroupMemberRepository;
+import com.group13.DalTalks.service.GroupMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/group_members")
 public class GroupMemberController {
   @Autowired
-  private GroupMemberRepository groupMemberRepository;
+  private GroupMemberService groupMemberService;
 
   //here we could create calls for adding group members, deleting group members, and getting group members
+
+  @PostMapping("/add-membership")
+  public GroupMembers createGroupMembers(@RequestBody GroupMembers groupMembers) {
+    return groupMemberService.saveGroupMember(groupMembers);
+  }
 }
