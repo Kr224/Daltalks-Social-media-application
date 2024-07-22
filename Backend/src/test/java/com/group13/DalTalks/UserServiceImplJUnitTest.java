92e09f0b4b56af97453e1b932dd4ec7f5de56b14
btran <bc740116@dal.ca>
2024-07-21 22:48:09 -0300

    feat: add test case for getAllUserExcept, getAllPendingUser, getAllUsers, getEmailByUserID

--- Backend/src/test/java/com/group13/DalTalks/UserServiceImplJUnitTest.java
+++ Backend/src/test/java/com/group13/DalTalks/UserServiceImplJUnitTest.java
@@ -1,13 +1,20 @@
 package com.group13.DalTalks;
 
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
+import com.group13.DalTalks.model.User;
 import com.group13.DalTalks.repository.UserRepository;
-import com.group13.DalTalks.service.Implementations.UserServiceImpl; // Import concrete implementation
+import com.group13.DalTalks.service.Implementations.UserServiceImpl;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,6 +27,42 @@ class UserServiceImplJUnitTest {
     @Autowired
     private UserServiceImpl userService; // Use concrete implementation
 
+    @Test
+    void getAllUserExcept() {
+        User user1 = new User("Alice@dal.ca", "Pending!123", "What is your mother's maiden name?", "Camille", null, "user");
+        userRepository.save(user1);
 
+        List<User> getAllUserExcept = userService.getAllUserExcept(user1.getId());
 
+        assertEquals(0, getAllUserExcept.size());
+    }
+
+    @Test
+    void getAllPendingUser() {
+        User user1 = new User("Alice@dal.ca", "Pending!123", "What is your mother's maiden name?", "Camille", "pending", "user");
+        userService.createUser(user1);
+
+        List<User> getAllPendingUser = userService.getAllPendingUser();
+
+        assertEquals(1, getAllPendingUser.size());
+    }
+
+    @Test
+    void getAllUsers() {
+        User user1 = new User("Alice@dal.ca", "pending!123", "What is your mother's maiden name?", "Camille", "pending", "user");
+        userRepository.save(user1);
+        List<User> getAllUser = userService.getAllUsers();
+
+        assertFalse(getAllUser.isEmpty());
+    }
+
+    @Test
+    void getEmailByUserID() {
+        User user1 = new User("Alice@dal.ca", "pending!123", "What is your mother's maiden name?", "Camille", "pending", "user");
+        userRepository.save(user1);
+
+        String userEmail = userService.getEmailByUserID(user1.getId());
+
+        assertEquals(user1.getEmail(), userEmail);
+    }
 }
