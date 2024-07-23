3ebb4bd8b34f2d1870e94c56e40ad694853d51db
btran <bc740116@dal.ca>
2024-07-23 17:07:03 -0300

    Feat: create test case for post function (getAllPost and getPostByID)

--- Backend/src/main/java/com/group13/DalTalks/service/Implementations/PostServiceImpl.java
+++ Backend/src/main/java/com/group13/DalTalks/service/Implementations/PostServiceImpl.java
@@ -1,12 +1,14 @@
 package com.group13.DalTalks.service.Implementations;
 
 import com.group13.DalTalks.model.Post;
+import com.group13.DalTalks.model.User;
 import com.group13.DalTalks.repository.PostRepository;
 import com.group13.DalTalks.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -21,7 +23,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public Post getPostById(int id){
-        return postRepository.findById(id).get();
-
+        Optional<Post> postOptional = postRepository.findById(id);
+        return postOptional.orElse(null);
     }
 }
