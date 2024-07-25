6e32aff8e72b9b4235a3d6165837c175c9102f3b
btran <bc740116@dal.ca>
2024-07-24 23:05:07 -0300

    Feat: refractor design code smell for getAllPendingUser and getAllUserExcept in UserServiceImpl, and PostServiceImplTest

--- Backend/src/main/java/com/group13/DalTalks/service/Implementations/UserServiceImpl.java
+++ Backend/src/main/java/com/group13/DalTalks/service/Implementations/UserServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 import java.util.regex.Pattern;
 import java.util.*;
+import java.util.stream.Stream;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -84,18 +85,18 @@ public class UserServiceImpl implements UserService {
     
     @Override
     public List<User> getAllUserExcept(int userID) {
-        return this.userRepository.findAll()
-                                   .stream()
-                                   .filter(user -> user.getId() != userID)
-                                   .collect(Collectors.toList());
+        List<User> getAllUsers = this.userRepository.findAll();
+        Stream<User> StreamUsers = getAllUsers.stream();
+        Stream<User> filterUsers = StreamUsers.filter(user -> user.getId() != userID);
+        return filterUsers.collect(Collectors.toList());
     }
 
     @Override
     public List<User> getAllPendingUser() {
-        return this.userRepository.findAll()
-                                .stream()
-                                .filter(user -> "pending".equals(user.getStatus()))
-                                .collect(Collectors.toList());
+        List<User> getAllUsers = this.userRepository.findAll();
+        Stream<User> StreamUsers = getAllUsers.stream();
+        Stream<User> filterUsers = StreamUsers.filter(user -> "pending".equals(user.getStatus()));
+        return filterUsers.collect(Collectors.toList());
     }
 
     @Override
