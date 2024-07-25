fa2ff00cbbe741e57e20d3cde827e95a9fa96017
btran <bc740116@dal.ca>
2024-07-24 23:11:11 -0300

    Fix: design smell of Unnecessary Abstraction and Unutilized Abstraction of file uitility in function folder

--- Backend/src/main/java/com/group13/DalTalks/Function/utility.java
+++
@@ -1,9 +0,0 @@
-package com.group13.DalTalks.Function;
-
-import com.group13.DalTalks.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class utility {
-    UserService userService;
-
-}
